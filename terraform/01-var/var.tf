e68136ed35111bc6f9e80aa232aba6f2126a24e0
SACHIN <sachinskbirajdar@gmail.com>
2023-10-08 20:39:03 +0530

    chnges

--- terraform/01-var/var.tf
+++ terraform/01-var/var.tf
@@ -7,3 +7,25 @@ output "sample" {
   
   value = var.sample  #printing varibale
 }
+
+output "sample1" {
+  
+
+value = "The value that i am printing is $(var.sample)"
+}
+
+variable "ex-list" {
+
+  default = [
+    "cloud",
+    "devops",
+    100,
+    "Sachin"
+  ]
+  
+}
+
+output "ec-list-output" {
+value = " welcome to ${var.ex-list[0]} course is ${var.ex-list[1]} duration is ${var.ex-list[2]}"
+
+}
